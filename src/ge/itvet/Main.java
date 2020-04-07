cfef91ae6020d8d4f2a19d7e681fe69eabe9c059
mishaItvet <mchelidze@itvet.ge>
2020-04-07 21:08:04 +0400

    ignore iml file

--- src/ge/itvet/Main.java
+++ src/ge/itvet/Main.java
@@ -3,7 +3,5 @@ package ge.itvet;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello github");
-        System.out.println("hello from chxei");
     }
 }
